bae6c3ed727c12c47c6bc41a7083ec506876fdb7
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2024-08-14 11:45:35 +0530

    add css

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <img src='../images/tech-logo-footer.png' alt='' />
             <p>Busan is an amazing magazine Blogger theme that is easy to customize for your needs</p>
             <i className='fa fa-envelope'></i>
-            <span> hello@beautiful.com </span> <br />
+            <span> hello@flower.com </span> <br />
             <i className='fa fa-headphones'></i>
             <span> +91 60521488</span>
           </div>
@@ -60,7 +60,7 @@ const Footer = () => {
         <div className='container flexSB'>
           <p>© all rights reserved</p>
           <p>
-            made with <i className='fa fa-heart'></i> by gorkhcoder
+            made with <i className='fa fa-heart'></i> by Keerthana Duraisamy
           </p>
         </div>
       </div>
